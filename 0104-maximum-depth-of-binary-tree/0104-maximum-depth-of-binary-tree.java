52a3f210db78ddc9aaf3cb0507af3b3da35f61b7
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-08-11 12:41:21 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (28.64%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,13 +14,14 @@
  * }
  */
 class Solution {
-    // public int maxDepth(TreeNode root) {
-    //     if(root == null)
-    //         return 0;
-    //     else return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-    // }
     
-    public int maxDepth(TreeNode root){
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        else return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+    
+    public int maxDepthWithQueuev(TreeNode root){
         if(root == null)
             return 0;
         Queue<TreeNode> queue = new LinkedList<>();
