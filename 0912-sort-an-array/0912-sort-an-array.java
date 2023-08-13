0573e2e64ffd5c7852a9c9d52c5b8cd5f7f6c26c
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-08-13 16:22:23 -0400

    Time: 21 ms (84.87%), Space: 54.7 MB (87.37%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,58 +1,90 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        //return quickSort(nums, 0, nums.length - 1);
-        shellSort(nums, nums.length);
+        if(nums.length < 1) return null;
+        if(nums.length < 15)
+            shellSort(nums, nums.length);
+          //quickSort(nums,0,nums.length -1);
+        else{
+            if(nums.length < 2) return nums;
+            int mid = nums.length / 2;
+            int[] left = Arrays.copyOfRange(nums, 0, mid);
+            int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+            return mergeSort(sortArray(left), sortArray(right));
+            
+        } 
         return nums;
     }
     
-//     public static int[] quickSort(int[] array, int start, int end){
-//         if(array.length < 1 || start < 0 || end >= array.length || start > end) return null;
-        
-//         /*分区指示器*/
-//         int zoneIndex = partition(array, start, end);
-//         if(zoneIndex > start){
-//             quickSort(array, start, zoneIndex - 1);
-//         }
-//         if(zoneIndex < end){
-//             quickSort(array, zoneIndex + 1, end);
-//         }
-//         return array;
-//     }
     
-//     /*分区方法*/
-//     public static int partition(int[] array, int start, int end){
-//         // one element left
-//         if(start == end) return start;
-//         // choose the rightmost element as pivot
-//         int pivot = array[end];
-//         int zoneIndex = start - 1;
-
-//         for(int i = start; i <= end; i++){
-//             if(array[i] <= array[end]){
-//                 zoneIndex ++;
-//                 if(i > zoneIndex)
-//                     swap(array, i , zoneIndex);
-//             }
-//         }
-//         return zoneIndex;
-//     }
-    
-//     public static void swap(int[] array, int i, int j){
-//         int temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-//     }
-    
-      void shellSort(int array[], int n) {
-          for (int interval = n / 2; interval > 0; interval /= 2) {
-                for (int i = interval; i < n; i += 1) {
-                    int temp = array[i];
-                    int j;
-                    for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
-                      array[j] = array[j - interval];
-                    }
-                array[j] = temp;
+    //shellSort
+    void shellSort(int[] array, int n) {
+        for (int interval = n / 2; interval > 0; interval /= 2) {
+            for (int i = interval; i < n; i += 1) {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+                    array[j] = array[j - interval];
                 }
-          }
+                array[j] = temp;
+            }
+         }
      }
+    
+    //qucikSort
+    void quickSort(int[] array, int start, int end){
+        if(start < end){
+            int pivot = partition(array, start, end);
+            quickSort(array, start, pivot - 1);
+            quickSort(array, pivot + 1, end);
+        }
+    }
+    static int partition(int[] array, int start, int end){
+        // choose the rightmost element as pivot
+        int pivot = array[end];
+        //pointer
+        int i = start - 1;
+        
+        for(int j = start; j < end; j++){
+            if(array[j] <= pivot){
+                // if element smaller than pivot is found
+                // swap it with the element at i with element at j
+                i++;
+                swap(array, i, j);
+            }
+        }
+        
+        // swap the pivot element with the greater element specified by i
+        i++;
+        swap(array, i, end);
+        
+        // return the position from where partition is done
+        return i;
+    }
+    
+    static void swap(int[] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    //mergeSort
+    public static int[] mergeSort(int[] left, int[] right){
+        int[] res = new int[left.length + right.length];
+        for (int index=0, i=0, j=0; index < res.length; index++){
+            if(i >= left.length){
+                res[index] = right[j];
+                j++;
+            }else if(j >= right.length){
+                res[index] = left[i];
+                i++;
+            }else if (left[i] > right[j]){
+                res[index] = right[j];
+                j++;
+            }else{
+                res[index] = left[i];
+                i++;
+            }
+        }
+        return res;
+    }   
 }
