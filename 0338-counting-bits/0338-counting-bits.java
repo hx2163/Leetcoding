9a1e631bebf2caf0b828eaf1648fe1b47207be5d
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-08-18 15:53:32 -0400

    Time: 2 ms (68.32%), Space: 46.5 MB (41.62%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,27 @@
 class Solution {
-    public int[] countBits(int n) {
+    
+    // x&(x-1)
+    public int[] countBits2(int n) {
         int[] bits = new int[n+1];
+        bits[0] = 0;
         for(int i=1; i<=n; i++){
             bits[i] = bits[i&(i-1)] + 1;
         }
         return bits;
     }
+    
+    //odd or even
+    public int[] countBits(int n) {
+        int[] bits = new int[n+1];
+        bits[0] = 0;
+        for(int i=1; i<=n; i++){
+            if((i&1) == 1){ // is odd
+                bits[i] = bits[i-1] + 1;
+            }else{ //is even: number of 1's = bit[i]/2
+                bits[i] = bits[i>>1];
+            }
+        }
+        return bits;
+    }    
+    
 }
