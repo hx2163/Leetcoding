bbb5c57d472aaf064c0de18870d37d76e60dfc79
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-08-23 17:53:15 -0400

    Time: 4 ms (100.00%), Space: 42.8 MB (56.47%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,4 +1,20 @@
 class Solution {
+    
+    public boolean isPalindrome(int num){
+       if(num == 0) return true;
+       if(num < 0 || num % 10 == 0) return  false; 
+        
+       int reversed = 0, remainder, original = num;
+        
+       while(num != 0) {   
+            remainder = num % 10; 
+            reversed = reversed * 10 + remainder;
+            num  /= 10;  //the last digit is removed from num after division by 10.
+        }
+        // palindrome if original and reversed are equal
+        return original == reversed;
+    }
+    
     /*
     store one half of the integer in a another variable in reversed order.
     */
@@ -17,22 +33,6 @@ class Solution {
         }
         // Handle both odd-length and even-length palindromes
         return (x==rev || x==rev/10);
-    }
-    
-    
-    public boolean isPalindrome(int num){
-       if(num == 0) return true;
-       if(num < 0 || num % 10 == 0) return  false; 
-        
-       int reversed = 0, remainder, original = num;
-        
-       while(num != 0) {   
-            remainder = num % 10; 
-            reversed = reversed * 10 + remainder;
-            num  /= 10;  //the last digit is removed from num after division by 10.
-        }
-        // palindrome if original and reversed are equal
-        return original == reversed;
     }
     
 }
