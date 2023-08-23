4ceff3b87fbc9cc5f1ea7fd11422d4935a51aaa4
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-08-23 18:21:56 -0400

    Time: 3 ms (90.76%), Space: 43.4 MB (95.53%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    private int helper(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;            
+            case 'M': return 1000;
+            default : return 0;          
+        }
+    }
+    
+    public int romanToInt(String s) {
+        int result = 0;
+        
+        for(int i=0; i < s.length(); i++){
+            char front = s.charAt(i);
+            if((i+1)>s.length()-1){
+                result += helper(front);
+                break;
+            }
+            char back = s.charAt(i+1);
+            if(helper(back) > helper(front))
+                result -= helper(front);
+            else
+                result += helper(front);
+        }
+        
+        return result;
+        
+    }
+}
