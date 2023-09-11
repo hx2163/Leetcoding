8d3de33c2692e2a7e6148ca96fafac2bcc95d816
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-09-11 00:15:24 -0400

    Time: 21 ms (83.98%), Space: 54.7 MB (82.36%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -5,7 +5,9 @@ class Solution {
             shellSort(nums, nums.length);
             return nums;
         }
-          //quickSort(nums,0,nums.length -1);
+          //  quickSort(nums,0,nums.length -1);
+          //  HeapSort(nums);
+          //  return nums;
         else{
             if(nums.length < 2) return nums;
             int mid = nums.length / 2;
@@ -13,10 +15,13 @@ class Solution {
             int[] right = Arrays.copyOfRange(nums, mid, nums.length);
             return mergeSort(sortArray(left), sortArray(right));            
         } 
+
     }
     
+
     
-    //shellSort
+    
+/****************Shell Sort*********************/
     void shellSort(int[] array, int n) {
         for (int interval = n / 2; interval > 0; interval /= 2) {
             for (int i = interval; i < n; i += 1) {
@@ -29,8 +34,10 @@ class Solution {
             }
          }
      }
+
     
-    //qucikSort
+    
+/****************Quick Sort*********************/
     void quickSort(int[] array, int start, int end){
         if(start < end){
             int pivot = partition(array, start, end);
@@ -66,8 +73,10 @@ class Solution {
         array[i] = array[j];
         array[j] = temp;
     }
+
     
-    //mergeSort
+    
+/****************mergeSort*********************/
     public static int[] mergeSort(int[] left, int[] right){
         int[] res = new int[left.length + right.length];
         for (int index=0, i=0, j=0; index < res.length; index++){
@@ -86,5 +95,53 @@ class Solution {
             }
         }
         return res;
-    }   
+    }
+    
+    
+    
+    
+/****************Heap Sort*********************/
+    void HeapSort(int[] arr){
+        int N = arr.length;
+     
+        // Build heap (rearrange array)
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+        
+        // One by one extract an element from heap
+        for (int i = N - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        } 
+    }
+    
+    
+    static void heapify(int[] arr, int N, int i){      
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);        
+        }               
+    }
 }
