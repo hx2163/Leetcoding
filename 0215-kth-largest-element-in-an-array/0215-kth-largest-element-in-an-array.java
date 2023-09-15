97857e1fb5381a9ce81ea0b303bb57504d2e793b
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-09-14 23:31:59 -0400

    Time: 12 ms (91.44%), Space: 55.7 MB (42.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,57 +1,39 @@
 public class Solution {
     
     public int findKthLargest(int[] nums, int k) {
-
-        return select(nums, k-1);
-    }
-    
-    // Quick select
-    private int select(int[] nums, int k) {
-        int left = 0, right = nums.length-1;
-        while(true) {
-            if(left == right)
-                return nums[left];
-            int pivotIndex = medianOf3(nums, left, right);
-            pivotIndex = partition(nums, left, right, pivotIndex);
-            if(pivotIndex == k)
-                return nums[k];
-            else if(pivotIndex > k)
-                right = pivotIndex-1;
-            else
-                left = pivotIndex+1;
-        }
-    }
-    
-    //Use median-of-three strategy to choose pivot
-    private int medianOf3(int[] nums, int left, int right) {
-        int mid = left + (right - left) / 2;
-        if(nums[right] > nums[left])
-            swap(nums, left, right);
-        if(nums[right] > nums[mid])
-            swap(nums, right, mid);
-        if(nums[mid] > nums[left])
-            swap(nums,left, mid);
-        return mid;
-    }
-    
-    private int partition(int[] nums, int left, int right, int pivotIndex) {
-        int pivotValue = nums[pivotIndex];
-        swap(nums, pivotIndex, right);
-        int index = left;
-        for(int i = left; i < right; ++i) {
-            if(nums[i] > pivotValue) {
-                swap(nums, index, i);
-                ++index;
+        k = nums.length - k;
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int j = partition(nums, l, h);
+            if (j == k) {
+                break;
+            } else if (j < k) {
+                l = j + 1;
+            } else {
+                h = j - 1;
             }
         }
-        swap(nums, right, index);
-        return index;
+        return nums[k];
     }
-    
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+
+    private int partition(int[] a, int l, int h) {
+        int i = l, j = h + 1;
+        while (true) {
+            while (a[++i] < a[l] && i < h) ;
+            while (a[--j] > a[l] && j > l) ;
+            if (i >= j) {
+                break;
+            }
+            swap(a, i, j);
+        }
+        swap(a, l, j);
+        return j;
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 
 }
