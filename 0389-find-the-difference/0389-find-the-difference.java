4e76868500bf2a24ad58a7a7787a7a85fcb450a5
Han Xu <90854945+hx2163@users.noreply.github.com>
2023-09-24 20:17:01 -0400

    Time: 1 ms (100.00%), Space: 41.2 MB (9.20%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,5 +1,6 @@
 class Solution {
     public char findTheDifference(String s, String t) {
+        //if we take XOR of every character. the similar char will cancel each other and the left the diff
         char c = 0;
         for(char cs : s.toCharArray()) c ^= cs;
         for(char ct : t.toCharArray()) c ^= ct;
