18cb475b5eb324bc985ba06db32c89f8282cb142
Han Xu <90854945+hx2163@users.noreply.github.com>
2024-10-08 13:33:19 -0400

    Time: 6 ms (94.54%), Space: 55 MB (66.42%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int totalFruit(int[] tree) {
+        int lastFruit = -1;
+        int secondLastFruit = -1;
+        int lastFruitCount = 0;
+        int currMax = 0;
+        int max = 0;
+        
+        for(int fruit : tree){
+            
+            //curr fruit is the same or not
+            if(fruit == lastFruit || fruit == secondLastFruit) 
+                currMax +=1;
+            else
+                currMax = lastFruitCount + 1;  
+            
+            //check if the lastFruitCount is the same or not
+            if(fruit == lastFruit)
+                lastFruitCount +=1;
+            else 
+                lastFruitCount = 1;
+            
+            if(fruit != lastFruit){
+                secondLastFruit = lastFruit;
+                lastFruit = fruit;
+            }
+            max = Math.max(max, currMax);
+        }
+        return max;
+        }
+}
+
