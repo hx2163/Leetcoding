80e80449cb5f139415c280f9dda3da248be32945
Han Xu <90854945+hx2163@users.noreply.github.com>
2024-10-08 13:12:05 -0400

    Time: 2 ms (34.87%), Space: 44.5 MB (43.11%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -5,10 +5,11 @@ class Solution {
         int anchor = 0;
         
         for(int i=0; i < nums.length; i++){
+            //if decresing set anchor
             if(i > 0 && nums[i-1] >= nums[i]){
                 anchor = i;
             }
-            res = Math.max(res, i - anchor + 1);
+            res = Math.max(res, i + 1 - anchor );
         }        
         return res;
     }
