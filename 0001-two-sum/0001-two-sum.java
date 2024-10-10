159a7721fee5c744ea38a800d08d20043cb7b1fd
Han Xu <90854945+hx2163@users.noreply.github.com>
2024-10-09 21:34:02 -0400

    Time: 2 ms (98.79%), Space: 44.9 MB (31.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,20 @@
 class Solution {
+    
     public int[] twoSum(int[] nums, int target) {
-        /*
-        遍历nums把 target - nums[i]的值存在hashMap里面 as anotherIndex
-        如果anotherIndex != null 找到结果并返回
-        */
-        Map<Integer, Integer> storeNums = new HashMap<>(nums.length, 1);
-        int[] result = new int[2];
-        for (int i = 0; i< nums.length; i++){
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        
+        for(int i=0; i<nums.length; i++){
             int another = target - nums[i];
-            Integer anotherIndex = storeNums.get(another);
-            if (anotherIndex != null){
-                result[0] = anotherIndex;
-                result[1] = i;
+            Integer anotherIndex = map.get(another);
+            if(anotherIndex != null){
+                res[0] = anotherIndex;
+                res[1] = i;
                 break;
             }else{
-                storeNums.put(nums[i],i);
+                map.put(nums[i],i);
             }
         }
-        return result;
+        return res;
     }
 }
