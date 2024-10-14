970e59320e1021e06b4b53da0a73c51b32f41a7e
Han Xu <90854945+hx2163@users.noreply.github.com>
2024-10-14 14:52:26 -0400

    Time: 24 ms (75.95%), Space: 46.5 MB (44.56%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,43 @@
+//Brute Force
+// public class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int count = 0;
+        
+//         // 外层循环枚举子数组的起始位置
+//         for (int start = 0; start < nums.length; start++) {
+//             int sum = 0;
+//             // 内层循环枚举子数组的结束位置
+//             for (int end = start; end < nums.length; end++) {
+//                 sum += nums[end];
+                
+//                 // 检查当前子数组的和是否等于 k
+//                 if (sum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
 
+//Hash Map
 public class Solution {
     public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixSums = new HashMap<>(); //key: preSum, value: frequency
+        int sum = 0;
         int count = 0;
-        
-        // 外层循环枚举子数组的起始位置
-        for (int start = 0; start < nums.length; start++) {
-            int sum = 0;
-            // 内层循环枚举子数组的结束位置
-            for (int end = start; end < nums.length; end++) {
-                sum += nums[end];
-                
-                // 检查当前子数组的和是否等于 k
-                if (sum == k) {
-                    count++;
-                }
+         prefixSums.put(0,1); // ==> basecase empty subarray
+            
+        for(int num : nums){
+            sum += num;  
+            int diff = sum - k;
+            //if there is a prefixSum found in the map 
+            if(prefixSums.containsKey(diff)){
+                count +=  prefixSums.get(diff);
             }
+            //else add the sum into prefixSum map
+            prefixSums.put(sum, prefixSums.getOrDefault(sum, 0) + 1);
         }
         
         return count;
