b6ad386d7d8a0a599de80d43996baa6e783c5090
Han Xu <90854945+hx2163@users.noreply.github.com>
2024-10-14 15:12:17 -0400

    Time: 7 ms (40.31%), Space: 44.8 MB (34.61%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,42 +1,20 @@
 class Solution {
-    public int lengthOfLongestSubstring2(String s) {
-        Set set = new HashSet();
-        int max = 0;
-        int left = 0;
-        for (int right = 0; right < s.length(); right++) {
-            while(!set.add(s.charAt(right))) {
-                set.remove(s.charAt(left++));
-            }
-            max = Math.max(max, right - left + 1);
-        }
-        return max;
-    }
-    
     public int lengthOfLongestSubstring(String s) {
-        Set set = new HashSet();
-        int left = 0;
+        Set<Character> set = new HashSet<>();
+        
         int max = 0;
+        int left = 0;
         
-        if(s.length() == 0) return 0;
-        
-        for(int right = 0;right < s.length(); right++){
-            // if s[right] not in set => no repeating characters 
-            if(!set.contains(s.charAt(right))){
-                set.add(s.charAt(right)); //add s[right] to set
-                max = Math.max(set.size(), max); //if set size > max update the max
+        for(int r=0; r<s.length(); r++){
+            // 如果遇到重复字符，移动左指针直到窗口内无重复字符
+            while(!set.add(s.charAt(r))){
+                set.remove(s.charAt(left));
+                left++;
             }
-            //if s[right] is in set
-            else{
-                //move the pointer left and remove s[left] from set
-                while(set.contains(s.charAt(right))){
-                    set.remove(s.charAt(left));
-                    left++;
-                }
-                //after remove the repeat char then add s[right] into set
-                set.add(s.charAt(right));
-            }
+            //add char and update max
+            set.add(s.charAt(r));
+            max = Math.max(max, r-left+1);
         }
-        
         return max;
     }
 }
