205a481df29b6680c0d9b57085e7527ff9d0f0ad
Han Xu <90854945+hx2163@users.noreply.github.com>
2024-10-14 14:53:31 -0400

    Time: 24 ms (75.95%), Space: 46.6 MB (35.08%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,28 +1,28 @@
-//Brute Force
-// public class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         int count = 0;
-        
-//         // 外层循环枚举子数组的起始位置
-//         for (int start = 0; start < nums.length; start++) {
-//             int sum = 0;
-//             // 内层循环枚举子数组的结束位置
-//             for (int end = start; end < nums.length; end++) {
-//                 sum += nums[end];
-                
-//                 // 检查当前子数组的和是否等于 k
-//                 if (sum == k) {
-//                     count++;
-//                 }
-//             }
-//         }
-        
-//         return count;
-//     }
-// }
-
-//Hash Map
 public class Solution {
+    
+//Brute Force    
+    public int subarraySum1(int[] nums, int k) {
+        int count = 0;
+        
+        // 外层循环枚举子数组的起始位置
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            // 内层循环枚举子数组的结束位置
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                
+                // 检查当前子数组的和是否等于 k
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+ 
+    
+//Hash Map
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> prefixSums = new HashMap<>(); //key: preSum, value: frequency
         int sum = 0;
